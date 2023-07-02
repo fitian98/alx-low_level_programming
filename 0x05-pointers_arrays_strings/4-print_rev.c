d4cad02cfd2b673e895e653bfa9460e5f7dee90d
fitian98 <chaimaefitian@gmail.com>
2023-07-02 04:57:17 -0700

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void print_rev(char *s)
+{
+if (s != NULL)
+{
+int length = 0;
+int i;
+while (*(s + length) != '\0')
+{
+length++;
+}
+for (i = length - 1; i >= 0; i--)
+{
+putchar(*(s + i));
+}
+putchar('\n');
+}
+}
+
+
