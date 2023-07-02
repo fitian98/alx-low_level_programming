88cdf247a4cfe9116b8dbd32b989b15ec8c6e859
fitian98 <chaimaefitian@gmail.com>
2023-07-02 04:49:02 -0700

    test

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
+/**
+* _puts - Prints a string followed by a newline character to stdout.
+* @str: The string to be printed.
+*
+* Description: This function prints each character of the given string,
+*              followed by a newline character, to the standard output.
+*              If the pointer to the string is NULL, the function does nothing.
+*/
+
 void _puts(char *str)
 {
-if (str != NULL) {
-while (*str != '\0') {
+if (str != NULL) 
+{
+while (*str != '\0')
+{
 putchar(*str);
 str++;
 }
